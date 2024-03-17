a114f50ec62a1775afbb63113f58e04fa8cc54ed
thanhd <danhthanh418@gmail.com>
2024-03-17 09:48:03 +0700

    update: update fix card film ratio

--- src/components/ui/film/film.card.tsx
+++ src/components/ui/film/film.card.tsx
@@ -12,9 +12,9 @@ export const FilmCard = (props: IMovieBase) => {
   return (
     <Link className="font-semibold" href={filmLink} title={name}>
       <Card className="w-full max-w-sm rounded-lg border shadow-sm">
-        <div className="relative group overflow-hidden">
+        <div className="relative group overflow-hidden aspect-[2/3]">
           <Image
-            className="object-cover rounded-t-lg w-full h-full transition-all group-hover:scale-105"
+            className="object-cover rounded-t-lg transition-all group-hover:scale-105"
             alt="Film poster"
             width={400}
             height={600}
