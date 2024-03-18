bc9bee34ff669122d93022fc9167c1a2e152e94c
thanhd <danhthanh418@gmail.com>
2024-03-18 11:58:47 +0700

    update: update remove suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { Suspense } from "react";
-
 import { FilmCarousel } from "@/components/ui/film/film.carousel";
-import { FilmCarouselSkeleton } from "@/components/ui/film/film.carousel.skeleton";
 import FilmList from "@/components/ui/film/film.list";
-import FilmListSkeleton from "@/components/ui/film/film.list.skeleton";
 import { FilmPagination } from "@/components/ui/film/film.pagination";
 import { getFilms } from "@/lib/fetcher";
 import { calculatePageList } from "@/lib/pagination";
@@ -28,12 +24,8 @@ export default async function Home({ searchParams }: THomePageProps) {
   const pageList = calculatePageList(currentPage, PAGE_TO_DISPLAY, totalPage);
   return (
     <main className="container space-y-4">
-      <Suspense fallback={<FilmCarouselSkeleton />}>
-        <FilmCarousel items={items} />
-      </Suspense>
-      <Suspense fallback={<FilmListSkeleton />}>
-        <FilmList items={items} />
-      </Suspense>
+      <FilmCarousel items={items} />
+      <FilmList items={items} />
       <FilmPagination
         currentPage={currentPage}
         pageList={pageList}
