b224e5ef409c32bd3add4695ead82aa669b60614
thanhd <danhthanh418@gmail.com>
2024-03-22 20:00:40 +0700

    update: update priority and delete favicon

--- src/components/ui/film/film.card.tsx
+++ src/components/ui/film/film.card.tsx
@@ -23,6 +23,7 @@ export const FilmCard = (props: IMovieBase) => {
               objectFit: "cover",
             }}
             src={thumb_url}
+            priority={true}
           />
           <div className="absolute inset-0 flex items-center justify-center gap-2 pointer-events-none opacity-0 group-hover:opacity-100 transition-opacity">
             <PlayIcon className="w-16 h-16 scale-50 opacity-0 transition-all group-hover:scale-100 group-hover:opacity-100 bg-background bg-opacity-50 rounded-full p-4" />
