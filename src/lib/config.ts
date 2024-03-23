7c38fec9bc6296b23e1b82729d6bbce091d6381f
thanhd <danhthanh418@gmail.com>
2024-03-23 21:13:36 +0700

    add : add config.ts

---
+++ src/lib/config.ts
@@ -0,0 +1,11 @@
+import { loadEnvConfig } from "@next/env";
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+const config = {
+  UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL!,
+  UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN!,
+};
+
+export default config;
