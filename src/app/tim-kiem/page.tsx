0b05c5117c83caa5c7a97aa7315b1d3b20583176
thanhd <danhthanh418@gmail.com>
2025-01-06 22:30:31 +0700

    refactor: update SearchPage component to handle search parameters more robustly

--- src/app/tim-kiem/page.tsx
+++ src/app/tim-kiem/page.tsx
@@ -3,13 +3,15 @@ import SearchMovieTable from '@/components/ui/search/search-movie-table';
 import { Separator } from '@/components/ui/separator';
 import { searchFilms } from '@/lib/fetcher';
 
+type SearchPageProps = {
+  searchParams: Promise<{ keyword: string | string[] }>;
+};
 export default async function SearchPage({
   searchParams,
-}: {
-  searchParams: { keyword?: string };
-}) {
+}: Readonly<SearchPageProps>) {
   const { keyword = '' } = (await searchParams) || '';
-  const searchResult = await searchFilms(keyword);
+  const searchTerm = Array.isArray(keyword) ? keyword.join(' ') : keyword;
+  const searchResult = await searchFilms(searchTerm);
 
   if (!searchResult) {
     return <p>Không tìm thấy kết quả nào</p>;
@@ -19,14 +21,13 @@ export default async function SearchPage({
     <div className="flex flex-col gap-4">
       <SearchBreadcrumb
         breadcrumbData={{
-          keyword,
-          currentPage: searchResult.paginate.current_page,
+          keyword: searchTerm ?? '',
+          currentPage: searchResult.paginate.current_page ?? 1,
         }}
       />
       <Separator />
-      <h1>TimKiem</h1>
       {keyword ? (
-        <p className="uppercase">Kết quả tìm kiếm cho: {keyword}</p>
+        <h2 className="uppercase">Kết quả tìm kiếm cho: {keyword}</h2>
       ) : (
         <p>Vui lòng nhập từ khóa tìm kiếm</p>
       )}
