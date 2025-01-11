937e3cef89d0a08142bf5c529eef6acf6272df33
thanhd <danhthanh418@gmail.com>
2025-01-11 21:56:07 +0700

    refactor: improve pagination logic and adding documentation

--- src/lib/pagination.ts
+++ src/lib/pagination.ts
@@ -1,18 +1,31 @@
+/**
+ * Calculates a list of page numbers to display in pagination, centered around the current page
+ *
+ * @param currentPage - The currently active page number
+ * @param PAGE_TO_DISPLAY - Number of pages to show on each side of the current page
+ * @param totalPage - Total number of pages available
+ *
+ * @returns An array of page numbers to display
+ *
+ * @example
+ * // With 10 total pages, showing 2 pages on each side
+ * calculatePageList(5, 2, 10) // returns [3, 4, 5, 6, 7]
+ * calculatePageList(1, 2, 10) // returns [1, 2, 3]
+ * calculatePageList(10, 2, 10) // returns [8, 9, 10]
+ *
+ * // With 5 total pages, showing 1 page on each side
+ * calculatePageList(3, 1, 5) // returns [2, 3, 4]
+ */
 export const calculatePageList = (
   currentPage: number,
   PAGE_TO_DISPLAY: number,
   totalPage: number
 ): number[] => {
-  const pageList: number[] = [];
+  // Ensure start page is not less than 1
+  const start = Math.max(1, currentPage - PAGE_TO_DISPLAY);
+  // Ensure end page does not exceed total pages
+  const end = Math.min(totalPage, currentPage + PAGE_TO_DISPLAY);
 
-  for (
-    let i = currentPage - PAGE_TO_DISPLAY;
-    i < currentPage + PAGE_TO_DISPLAY;
-    i++
-  ) {
-    if (i < 1) continue;
-    if (i > totalPage) break;
-    pageList.push(i);
-  }
-  return pageList;
+  // Create array of page numbers from start to end
+  return Array.from({ length: end - start + 1 }, (_, index) => start + index);
 };
